1cdcec2c495e705b256e65d5038e3d98bfe6eaf5
Felipe Cruz <felipeldn91@gmail.com>
2022-05-04 17:52:39 +0100

    Show page working

--- lib/functions.php
+++ lib/functions.php
@@ -1,18 +1,32 @@
 <?php
 
-function get_pets()
-{
+function get_connection() {
     $config = require 'config.php';
 
-    $pdo = new PDO(
+    return new PDO(
         $config['database_dsn'],
         $config['database_user'],
         $config['database_pass']
     );
-    $result = $pdo->query('SELECT * FROM pet');
+};
+
+function get_pets()
+{
+    $pdo = get_connection();
+
+    $result = $pdo->query('SELECT * FROM pet LIMIT 3');
     $pets = $result->fetchAll();
 
     return $pets;
+}
+
+function get_pet($id)
+{
+    $pdo = get_connection();
+    $query = 'SELECT * FROM pet WHERE id = '.$id;
+    $result = $pdo->query($query);
+
+    return $result->fetch();
 }
 
 function save_pets($petsToSave) {
